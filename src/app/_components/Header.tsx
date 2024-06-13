cc3a18fe1ebcb9a59b016718e5171f507e884bc3
Anjani Chapparapu <anjani.chapparapu@bas.no>
2024-06-13 17:45:23 +0200

    fix: header background color on mobile

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -5,7 +5,7 @@ import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 
 export default function Header() {
 	return (
-		<header className='sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'>
+		<header className='sticky bg-white top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'>
 			<Sheet>
 				<SheetTrigger asChild>
 					<Button
