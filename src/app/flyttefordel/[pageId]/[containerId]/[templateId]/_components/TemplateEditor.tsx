ed347f390689c0146607abc04b3db0cad960541c
Anjani Chapparapu <anjani.chapparapu@bas.no>
2024-06-13 08:28:41 +0200

    feat: add template validation error message to template editor

--- src/app/flyttefordel/[pageId]/[containerId]/[templateId]/_components/TemplateEditor.tsx
+++ src/app/flyttefordel/[pageId]/[containerId]/[templateId]/_components/TemplateEditor.tsx
@@ -39,8 +39,9 @@ import {
 	FormField,
 	FormItem,
 	FormLabel,
+	FormMessage,
 } from '@/components/ui/form'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import Combobox from '@/components/form-control/Combobox'
 import { bytesToMb } from '@/lib/utils'
 import { updateTemplateAction } from '../_actions/updateTemplateAction'
@@ -80,16 +81,23 @@ type FormSchema = z.infer<typeof formSchema>
 
 const formSchema = z.object({
 	noExpiry: z.boolean(),
-	name: z.string().trim().min(1),
+	name: z.string().trim().min(1, 'Navn kan ikke være tomt'),
 	active: z.boolean(),
-	header: z.string().trim().min(1),
-	text: z.string().trim().min(1).max(256),
+	header: z.string().trim().min(1, 'Overskrift kan ikke være tomt'),
+	text: z.string().trim().min(1).max(256, 'Tekst kan maks være 256 tegn'),
 	logo: z
 		.object({
 			name: z.string().min(1),
 			url: z.string().min(1),
 		})
-		.or(z.string()),
+		.or(z.string())
+		.refine(val => {
+			if (typeof val === 'object') {
+				return val.name !== '' && val.url !== ''
+			}
+
+			return true
+		}, 'Et bilde/logo må legges til'),
 	validToDate: z.date(),
 	terms: z.string(),
 	offers: z
@@ -106,7 +114,7 @@ const formSchema = z.object({
 				templateOfferId: z.string(),
 			})
 		)
-		.min(1),
+		.min(1, 'Minst 1 tilbud må være lagt til'),
 })
 
 export default function TemplateEditor({
@@ -179,6 +187,10 @@ export default function TemplateEditor({
 		[form, params.pageId, template]
 	)
 
+	useEffect(() => {
+		form.trigger()
+	}, [])
+
 	return (
 		<Form {...form}>
 			<form
@@ -212,6 +224,8 @@ export default function TemplateEditor({
 													{...field}
 												/>
 											</FormControl>
+
+											<FormMessage />
 										</FormItem>
 									)}
 								/>
@@ -220,13 +234,15 @@ export default function TemplateEditor({
 									control={form.control}
 									name='active'
 									render={({ field }) => (
-										<FormControl>
-											<Switch
-												checked={field.value}
-												onCheckedChange={field.onChange}
-												disabled={!form.formState.isValid}
-											/>
-										</FormControl>
+										<FormItem>
+											<FormControl>
+												<Switch
+													checked={field.value}
+													onCheckedChange={field.onChange}
+													disabled={!form.formState.isValid}
+												/>
+											</FormControl>
+										</FormItem>
 									)}
 								/>
 							</CardContent>
@@ -241,30 +257,36 @@ export default function TemplateEditor({
 							</CardHeader>
 
 							<CardContent>
-								<FileUpload
-									imageUrl={
-										typeof templateLogoWatch === 'string'
-											? templateLogoWatch
-											: templateLogoWatch.url
-									}
-									onFileSelect={({ file, dataUrl }) => {
-										// Check if file is maximum 1 MB
-										if (parseFloat(bytesToMb(file.size)) > 1) {
-											form.setError('logo', {
-												message: 'Fil er for stor. Kan maks være 1 MB',
-											})
-											return
-										}
+								<FormField
+									control={form.control}
+									name='logo'
+									render={({ field }) => (
+										<FormItem>
+											<FileUpload
+												imageUrl={
+													typeof templateLogoWatch === 'string'
+														? templateLogoWatch
+														: templateLogoWatch.url
+												}
+												onFileSelect={({ file, dataUrl }) => {
+													// Check if file is maximum 1 MB
+													if (parseFloat(bytesToMb(file.size)) > 1) {
+														form.setError('logo', {
+															message: 'Fil er for stor. Kan maks være 1 MB',
+														})
+														return
+													}
 
-										form.setValue(
-											'logo',
-											{
-												name: file.name,
-												url: dataUrl,
-											},
-											{ shouldValidate: true, shouldDirty: true }
-										)
-									}}
+													field.onChange({
+														name: file.name,
+														url: dataUrl,
+													})
+												}}
+											/>
+
+											<FormMessage />
+										</FormItem>
+									)}
 								/>
 							</CardContent>
 						</Card>
@@ -283,6 +305,7 @@ export default function TemplateEditor({
 											<FormControl>
 												<Input {...field} />
 											</FormControl>
+											<FormMessage />
 										</FormItem>
 									)}
 								/>
@@ -299,6 +322,7 @@ export default function TemplateEditor({
 													rows={10}
 												/>
 											</FormControl>
+											<FormMessage />
 										</FormItem>
 									)}
 								/>
@@ -408,113 +432,125 @@ export default function TemplateEditor({
 					</CardHeader>
 
 					<CardContent>
-						<Table>
-							<TableHeader>
-								<TableRow>
-									<TableHead>Name</TableHead>
-									<TableHead className='max-w-32'>Tekst</TableHead>
-									<TableHead>Produkt</TableHead>
-									<TableHead>Produkt ID</TableHead>
-									<TableHead>Pris</TableHead>
-									<TableHead>
-										<span className='sr-only'>Handlinger</span>
-									</TableHead>
-								</TableRow>
-							</TableHeader>
-							<TableBody>
-								{templateOffersFieldArray.fields.map((offer, index) => (
-									<TableRow key={offer.id}>
-										<TableCell>
-											<FormField
-												control={form.control}
-												name={`offers.${index}.name`}
-												render={({ field }) => <Input {...field} />}
-											/>
-										</TableCell>
-										<TableCell>
-											<FormField
-												control={form.control}
-												name={`offers.${index}.text`}
-												render={({ field }) => <Textarea {...field} />}
-											/>
-										</TableCell>
-										<TableCell>
-											<FormField
-												control={form.control}
-												name={`offers.${index}.product`}
-												render={({ field }) => (
-													<Combobox
-														defaultValue={{
-															label: field.value.name,
-															value: field.value.productId,
-														}}
-														options={products.map(product => ({
-															label: product.name!,
-															value: product.productId!,
-														}))}
-														onSelect={option => {
-															field.onChange({
-																name: option.label,
-																productId: option.value,
-															})
-														}}
-													/>
-												)}
-											/>
-										</TableCell>
-										<TableCell>
-											{
-												templateOffersWatch.find(
-													templateOffer =>
-														templateOffer.templateOfferId ===
-														offer.templateOfferId
-												)?.product.productId
-											}
-										</TableCell>
-										<TableCell>
-											<FormField
-												control={form.control}
-												name={`offers.${index}.price`}
-												render={({ field }) => (
-													<Input
-														type='number'
-														{...field}
-														onChange={e => {
-															field.onChange(parseInt(e.target.value))
-														}}
-													/>
-												)}
-											/>
-										</TableCell>
-										<TableCell>
-											<DropdownMenu>
-												<DropdownMenuTrigger asChild>
-													<Button
-														aria-haspopup='true'
-														size={'icon'}
-														variant={'ghost'}
-													>
-														<MoreHorizontal className='h-4 w-4' />
-														<span className='sr-only'>Bruke meny</span>
-													</Button>
-												</DropdownMenuTrigger>
+						<FormField
+							control={form.control}
+							name='offers'
+							render={() => (
+								<FormItem>
+									<Table>
+										<TableHeader>
+											<TableRow>
+												<TableHead>Name</TableHead>
+												<TableHead className='max-w-32'>Tekst</TableHead>
+												<TableHead>Produkt</TableHead>
+												<TableHead>Produkt ID</TableHead>
+												<TableHead>Pris</TableHead>
+												<TableHead>
+													<span className='sr-only'>Handlinger</span>
+												</TableHead>
+											</TableRow>
+										</TableHeader>
+										<TableBody>
+											{templateOffersFieldArray.fields.map((offer, index) => (
+												<TableRow key={offer.id}>
+													<TableCell>
+														<FormField
+															control={form.control}
+															name={`offers.${index}.name`}
+															render={({ field }) => <Input {...field} />}
+														/>
+													</TableCell>
+													<TableCell>
+														<FormField
+															control={form.control}
+															name={`offers.${index}.text`}
+															render={({ field }) => <Textarea {...field} />}
+														/>
+													</TableCell>
+													<TableCell>
+														<FormField
+															control={form.control}
+															name={`offers.${index}.product`}
+															render={({ field }) => (
+																<Combobox
+																	defaultValue={{
+																		label: field.value.name,
+																		value: field.value.productId,
+																	}}
+																	options={products.map(product => ({
+																		label: product.name!,
+																		value: product.productId!,
+																	}))}
+																	onSelect={option => {
+																		field.onChange({
+																			name: option.label,
+																			productId: option.value,
+																		})
+																	}}
+																/>
+															)}
+														/>
+													</TableCell>
+													<TableCell>
+														{
+															templateOffersWatch.find(
+																templateOffer =>
+																	templateOffer.templateOfferId ===
+																	offer.templateOfferId
+															)?.product.productId
+														}
+													</TableCell>
+													<TableCell>
+														<FormField
+															control={form.control}
+															name={`offers.${index}.price`}
+															render={({ field }) => (
+																<Input
+																	type='number'
+																	{...field}
+																	onChange={e => {
+																		field.onChange(parseInt(e.target.value))
+																	}}
+																/>
+															)}
+														/>
+													</TableCell>
+													<TableCell>
+														<DropdownMenu>
+															<DropdownMenuTrigger asChild>
+																<Button
+																	aria-haspopup='true'
+																	size={'icon'}
+																	variant={'ghost'}
+																>
+																	<MoreHorizontal className='h-4 w-4' />
+																	<span className='sr-only'>Bruke meny</span>
+																</Button>
+															</DropdownMenuTrigger>
 
-												<DropdownMenuContent align='end'>
-													<DropdownMenuLabel>Handlinger</DropdownMenuLabel>
-													<DropdownMenuSeparator />
-													<DeleteTemplateOfferButton
-														templateOfferId={offer.templateOfferId}
-														afterDelete={() => {
-															templateOffersFieldArray.remove(index)
-														}}
-													/>
-												</DropdownMenuContent>
-											</DropdownMenu>
-										</TableCell>
-									</TableRow>
-								))}
-							</TableBody>
-						</Table>
+															<DropdownMenuContent align='end'>
+																<DropdownMenuLabel>
+																	Handlinger
+																</DropdownMenuLabel>
+																<DropdownMenuSeparator />
+																<DeleteTemplateOfferButton
+																	templateOfferId={offer.templateOfferId}
+																	afterDelete={() => {
+																		templateOffersFieldArray.remove(index)
+																	}}
+																/>
+															</DropdownMenuContent>
+														</DropdownMenu>
+													</TableCell>
+												</TableRow>
+											))}
+										</TableBody>
+									</Table>
+
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
 					</CardContent>
 
 					<CardFooter className='border-t px-4 py-2'>
